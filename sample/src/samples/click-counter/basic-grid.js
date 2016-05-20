c82c0f224fcfc5df11d93daf0727fd3b07ade372
Vegar Ringdal <vegar.ringdal@gmail.com>
2016-05-21 01:33:13 +0200

    test

---
+++ sample/src/samples/click-counter/basic-grid.js
@@ -0,0 +1,7 @@
+export class BasicUse {
+
+  myCollection = [{name:"person1", age:20},{name:"person4", age:19},{name:"person3", age:25}];
+  myCurrentEntity = {};   //current entity you link to inputs etc
+  myGrid = {};
+
+}
---
+++ src/config-builder.js
@@ -0,0 +1,28 @@
+/**
+* Plugin configuration builder
+*/
+export class ConfigBuilder {
+
+  globalResources = [];
+
+  useAll() : ConfigBuilder {
+    this.globalResources.push([
+        'vGrid/v-grid-contextmenu',
+        'vGrid/v-grid-row-repeat',
+        'vGrid/v-grid-header-cells-label',
+        'vGrid/v-grid-header-cells-filter-text',
+        'vGrid/v-grid-header-cells-filter-selection',
+        'vGrid/v-grid-header-cells-filter-checkbox',
+        'vGrid/v-grid-row-cells-image',
+        'vGrid/v-grid-row-cells-checkbox',
+        'vGrid/v-grid-row-cells-selection',
+        'vGrid/v-grid-row-cells-input',
+        'vGrid/v-grid-col',
+        'vGrid/v-grid-header-col',
+        'vGrid/v-grid-row-col',
+         'vGrid/v-grid.js',
+         'vGrid/v-grid-atts'
+    ]);
+    return this;
+  }
+}
